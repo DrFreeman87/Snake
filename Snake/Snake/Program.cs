9fc7024be67e3e4b9f676f813fd40c360bd12464
Viktor <victor.tsyfra@gmail.com>
2018-11-21 15:58:51 +0200

    Изучение списков

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,6 +17,19 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> NumList = new List<int>();
+            NumList.Add(0);
+            NumList.Add(1);
+            NumList.Add(2);
+
+            int x = NumList[0];
+            int y = NumList[1];
+            int z = NumList[2];
+
+            foreach(int i in NumList)
+            {
+                Console.WriteLine(i);
+            }
 
             Console.ReadLine();
         }
